8c4a8fbb616124a71679993f50e22054a9d74c6d
Alexander Kabolov <alexander.kabolov@gmail.com>
2020-06-11 23:34:32 +0300

    bottom tab navigator created

--- src/features/main/index.js
+++ src/features/main/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
+import FindJobScreen from 'src/features/main/screens/FindJobScreen';
+import SettingsScreen from 'src/features/main/screens/SettingsScreen';
+
+const Tabs = createBottomTabNavigator();
 
 export default () => {
   return (
-    <SafeAreaView>
-      <Text>MainScreen</Text>
-    </SafeAreaView>
+    <Tabs.Navigator initialRouteName={'FIND_JOB_SCREEN'}>
+      <Tabs.Screen
+        options={{ tabBarLabel: 'find' }}
+        name='FIND_JOB_SCREEN'
+        component={FindJobScreen}
+      />
+      <Tabs.Screen
+        options={{ tabBarLabel: 'settings' }}
+        name='SETTINGS_SCREEN'
+        component={SettingsScreen}
+      />
+    </Tabs.Navigator>
   );
 };
